ceca756aea92d2a1947f2f53f8d0c24f73c5fb28
Umidbekakumo <Umidbek2022@gmail.com>
2023-07-08 21:01:24 +0000

    pushing udemy mini project

---
+++ Udemy/BandnameGen.py
@@ -0,0 +1,11 @@
+##  Band name generator 
+
+print('Hello! Welcome to Band Name Generator!')
+
+city = (input( 'What city did you grow up in? ' ))
+
+pet = (input(' What is the name of your pet? '))
+
+print('Your band name could be ' + city + " " + pet )
+
+
